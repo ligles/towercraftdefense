c7d45642968657107d245b506aeafe9910bb436e
ligles <ligles@Numero5.kiim.fr>
2016-01-13 09:29:54 +0100

    merge to head

---
+++ towercraftdefense/src/towercraftdefense/environnement/Zone.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package towercraftdefense.environnement;
+
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author ligles
+ */
+public class Zone extends Rectangle implements Drawable{
+    
+    boolean isWalkable;
+    boolean isBuildable;
+    boolean isFarmable;
+    
+    
+    
+    
+    
+}
