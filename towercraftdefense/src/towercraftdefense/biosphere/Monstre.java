838e0fa7cc486e4a2e83af32ae0f95812baa9d23
Othman <Othman@pc-Othman>
2016-01-13 11:39:15 +0100

    Merge origin/base into base
    Conflicts:
    	towercraftdefense/src/towercraftdefense/cycle/Cycle.java

--- towercraftdefense/src/towercraftdefense/biosphere/Monstre.java
+++ towercraftdefense/src/towercraftdefense/biosphere/Monstre.java
@@ -6,6 +6,38 @@ import java.awt.Image;
  * Created by SDOUGAMEHDI on 12/01/2016.
  */
 public class Monstre extends Personnage{
+
+    public double getXmonstre() {
+        return xmonstre;
+    }
+
+    public void setXmonstre(double xmonstre) {
+        this.xmonstre = xmonstre;
+    }
+
+    public double getYmonste() {
+        return ymonste;
+    }
+
+    public void setYmonste(double ymonste) {
+        this.ymonste = ymonste;
+    }
+
+    public Image getImgmonstre() {
+        return imgmonstre;
+    }
+
+    public void setImgmonstre(Image imgmonstre) {
+        this.imgmonstre = imgmonstre;
+    }
+    
+    public double xmonstre;
+    public double ymonste;
+    public Image imgmonstre;
+    
+    public Monstre(double xmonstre, double ymonste, Image imgmonstre) {
+        super(xmonstre, ymonste, imgmonstre);
+    }
     
     
 }
